c141eee514cf7aa1fbc3be983f84e41b866380c2
agung96tm <agung.96tm@gmail.com>
2019-08-12 19:05:11 +0700

    make funcional_tests an app, use LiveServerTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,65 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+
+import time
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class NewVistorTest(LiveServerTestCase):
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+
+  def tearDown(self):
+    self.browser.quit()
+
+  def check_for_now_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn(row_text, [row.text for row in rows])
+
+  def test_can_start_a_list_and_retrive_it_later(self):
+    # Edit has heart about a cool new online to-do app. She
+    # goes to checkout its homepage
+    self.browser.get(self.live_server_url)
+
+    # She notices the page title and header mention to-do lists
+    self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
+
+    # She is invited to enter a to-do item straight away
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
+
+    # She type "Buy peacock feathers" into a text box (Edith's hobby 
+    # is typing fly-fishing lures)
+    inputbox.send_keys('Buy peacock feathers')
+
+    # When she hits enter, the page updates, and now the page lists
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+    self.check_for_now_in_list_table('1: Buy peacock feathers')
+
+    # There is still a text box inviting her to add another item. She 
+    # enters "Use peacock feathers to make a fly" (Edit is very
+    # methodical)
+    input = self.browser.find_element_by_id('id_new_item')
+    input.send_keys('Use peacock feathers to make a fly')
+    input.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    # The page updates again, and now shows both items on her list
+    self.check_for_now_in_list_table('1: Buy peacock feathers')
+    self.check_for_now_in_list_table('2: Use peacock feathers to make a fly')
+
+    # Edith wonders whether the site will remember her list. Then she sees
+    # that the site has generated a unique url for her -- there is some
+    # explanatory text to that effect.
+    self.fail('Finish the test!')
+
+    # She visits that url - her to-do list is still there.
+    [...]
