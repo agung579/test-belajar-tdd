d6f01f8e6ff75c7c3e1fe12ad294354da1e2cd96
agung96tm <agung.96tm@gmail.com>
2019-08-15 21:44:46 +0700

    new URL + view for adding to existsing lists. FT passes :-)

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,6 +19,9 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
+    path('lists/new/', views.new_list, name = 'new_list'),
+    path('lists/<int:list_id>/add_item/', views.add_item, name = 'add_item'),
+    path('lists/<int:list_id>/', views.view_list, name = 'view_list'),
     path('', views.home_page, name = 'home'),
     # path('admin/', admin.site.urls),
 ]
